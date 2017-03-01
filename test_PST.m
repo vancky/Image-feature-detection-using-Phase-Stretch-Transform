a9f0645518726cf4d71818a1d548bf3e0c6acf02
vancky <vanckyli@gmail.com>
2017-03-01 20:49:01 +0800

    add astronomical test

--- test_PST.m
+++ test_PST.m
@@ -1,39 +1,25 @@
-% test script to test PST function
-
-clc  % clear screen
-clear  % clear all variables
-  % close all figures
-
-% import original image
-Image_orig=imread('lena_gray_512.tif');
-
-% if image is a color image, convert it to grayscale
-try
-    Image_orig=rgb2gray(Image_orig);
-catch
-end
-
+file_path='E:\Observation data\20131101\';
+filename='TaiWei-004JI.fit';
+Image_orig=fitsread([file_path filename]);
+%%
 %show the original image
 subplot(1,2,1)
-imshow(Image_orig)
+imshow(Image_orig,[]);
 title('Original Image')
-
-% convert the grayscale image do a 2D double array
-Image_orig=double(Image_orig);
-
-% low-pass filtering (also called localization) parameter
+%%
 handles.LPF=0.21; % Gaussian low-pass filter Full Width at Half Maximum (FWHM) (min:0 , max : 1)
 
 % PST parameters
-handles.Phase_strength=0.48;  % PST  kernel Phase Strength
-handles.Warp_strength=12.14;  % PST Kernel Warp Strength
-
+%handles.Phase_strength=0.48;  % PST  kernel Phase Strength
+%handles.Warp_strength=12.14;  % PST Kernel Warp Strength
+handles.Phase_strength=10;  % PST  kernel Phase Strength
+handles.Warp_strength=100; 
 % Thresholding parameters (for post processing)
-handles.Thresh_min=-1;      % minimum Threshold  (a number between 0 and -1)
-handles.Thresh_max=0.0019;  % maximum Threshold  (a number between 0 and 1)
+handles.Thresh_min=-0.001;      % minimum Threshold  (a number between 0 and -1)
+handles.Thresh_max=0;  % maximum Threshold  (a number between 0 and 1)
 
 % choose to compute the analog or digital edge
-Morph_flag = 1 ; %  Morph_flag=0 to compute analog edge and Morph_flag=1 to compute digital edge.
+Morph_flag = 0 ; %  Morph_flag=0 to compute analog edge and Morph_flag=1 to compute digital edge.
 
 % Apply PST and find features (sharp transitions)
 [Edge PST_Kernel]= PST(Image_orig,handles,Morph_flag);
@@ -61,9 +47,3 @@ figure
 [D_PST_Kernel_x D_PST_Kernel_y]=gradient(PST_Kernel);
 mesh(sqrt(D_PST_Kernel_x.^2+D_PST_Kernel_y.^2))
 title('PST Kernel phase Gradient')
-
-
-
-
-
-
